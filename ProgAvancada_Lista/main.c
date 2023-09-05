b377a6eb83ea42c355758b9ce29955a3237515cb
Victor Hugo Ferreira <victor.hugo.lopes.ferreira@gmail.com>
2023-09-05 20:16:10 -0300

    Lista enfileirar

--- ProgAvancada_Lista/main.c
+++ ProgAvancada_Lista/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+
+// 345
+
+typedef struct no{
+	int valor;
+	struct no *proximo;
+} No;
+
+typedef struct {
+	int tamanho;
+	No *inicio;
+	No *fim;
+} Fila;
+
+void enfileirar(No **fila, int valor){
+	No *aux, *novo = malloc(sizeof(No));
+	if(novo){
+		novo->valor = valor;
+		novo->proximo = NULL;
+		if(*fila == NULL) *fila = novo;
+		else{
+			aux = *fila;
+			while(aux->proximo) aux = aux->proximo;
+			aux->proximo = novo;
+		}
+	}else{
+		printf("Erro ao alocar memoria");
+	}
+}
+
+/*void enfileirar(Fila *fila, int valor){
+	No* no = malloc(sizeof(No));
+	no->valor = valor;
+	no->proximo = fila->fim;
+	if(!fila->inicio){
+		fila->inicio = no;
+	}
+	fila->fim = no;
+	fila->tamanho++;
+}*/
+
+/*void desenfileirar(Fila *fila){
+	No* no =
+	if(fila->inicio){
+
+	}else{
+		printf("Não há elementos na fila.")
+		getch();
+	}
+}*/
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
 }
